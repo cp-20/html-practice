a909479c1330742c74a7180874bc1f05b9d799d6
mario-hsp <47262658+mario-hsp@users.noreply.github.com>
2021-02-19 22:54:36 +0900

    main

--- js/anchor.js
+++ js/anchor.js
@@ -2,7 +2,6 @@
 	document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-				console.log('link');
 
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
