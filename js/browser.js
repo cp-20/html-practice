116530fcc2a24f72eb69d04df5fecb39dcc24081
mario-hsp <47262658+mario-hsp@users.noreply.github.com>
2021-02-20 14:40:15 +0900

    main

--- js/browser.js
+++ js/browser.js
@@ -1,7 +1,7 @@
 {
 	const browser = navigator.userAgent.toLowerCase();
 	if (browser.indexOf('msie') != -1 || browser.indexOf('trident') != -1) {
-		alert('Internet Explorerでは表示が正しくなされない場合があります', 'うぇーい')
+		alert('Internet Explorerでは表示が正しくなされない場合があります')
 	}
 
 	let devtools = function() {};
